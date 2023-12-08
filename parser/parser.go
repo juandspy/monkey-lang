10ed9a5b70749c9037d879fe5ce86c1870b50174
Juan Diaz Suarez <jdiazsua@redhat.com>
2023-12-08 16:20:57 +0100

    basic support for parsing let statements

--- parser/parser.go
+++ parser/parser.go
@@ -28,5 +28,69 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		// if the token is not an IDENT, return nil
+		return nil
+	} // expectPeek already calls p.nextToken() so that we skip the 'let' part
+
+	// assign the name of the identifier before incrementing the position again
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		// if the token is not a '=', then the statement is invalid f.e `let x y z`
+		return nil
+	} // expectPeek called p.nextToken() again
+
+	// TODO: We're skipping the expressions until we encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+// curTokenIs checks whether the current token is of a given type
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+// peekTokenIs checks whether the peek token is of a given type
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+// expectPeek checks whether the peekToken is of a given type and advances the positions.
+// This is useful for ensuring the order of the tokens inside the statement is correct.
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
 }
