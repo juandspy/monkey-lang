bb09fe969591f6c9721edf36a5119c64155cf3d1
Juan Diaz Suarez <jdiazsua@redhat.com>
2023-12-13 13:42:05 +0100

    parse expressions: integers

--- ast/ast.go
+++ ast/ast.go
@@ -120,3 +120,13 @@ func (es *ExpressionStatement) String() string {
 	}
 	return ""
 }
+
+// IntegerLiteral is just an integer expression like `5;`
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode()      {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string       { return il.Token.Literal }
